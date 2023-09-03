16c633c0dfb3e7cb4585809a01ae9b256e096487
AbdelRahman wadi <127618783+AbdElrhmanmwadi@users.noreply.github.com>
2023-09-03 12:03:48 +0300

    change in ui

--- lib/presentation/QuotesScreen.dart
+++ lib/presentation/QuotesScreen.dart
@@ -23,21 +23,21 @@ List<Tag>? listTage;
 Results? resuilt;
 
 class _QuotesScreenState extends State<QuotesScreen> {
-  ScrollController _scrollController = ScrollController();
+  final ScrollController _scrollController = ScrollController();
 
-  Future<void> _loadFavoriteQuoteStatus() async {
-    setState(() {
-      _isFavorite =
-          SharedPrefController().getData(key: _currentIndex.toString());
-    });
-  }
+  // Future<void> _loadFavoriteQuoteStatus() async {
+  //   setState(() {
+  //     _isFavorite =
+  //         SharedPrefController().getData(key: _currentIndex.toString());
+  //   });
+  // }
 
-  void _toggleFavorite() async {
-    setState(() {
-      _isFavorite = !_isFavorite;
-      SharedPrefController().setData(_currentIndex.toString(), _isFavorite);
-    });
-  }
+  // void _toggleFavorite() async {
+  //   setState(() {
+  //     _isFavorite = !_isFavorite;
+  //     SharedPrefController().setData(_currentIndex.toString(), _isFavorite);
+  //   });
+  // }
 
   int page = 2;
 
@@ -79,32 +79,32 @@ class _QuotesScreenState extends State<QuotesScreen> {
             builder: (context) => QuotesRandomScreen(),
           ));
         },
-        child: Icon(Icons.crisis_alert_outlined),
+        child: const Icon(Icons.crisis_alert_outlined),
       ),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
         elevation: 0,
-        actions: [
-          IconButton(
-              icon: Icon(
-                _isFavorite ? Icons.favorite : Icons.favorite_border,
-                color: Colors.red,
-              ),
-              onPressed: _toggleFavorite),
-          IconButton(
-              icon: Icon(
-                Icons.share,
-                color: Colors.grey[700],
-              ),
-              onPressed: () async {
-                QuoteController.shareQuote(lists, _currentIndex);
-              }),
-        ],
+       // actions: [
+          // IconButton(
+          //     icon: Icon(
+          //       _isFavorite ? Icons.favorite : Icons.favorite_border,
+          //       color: Colors.red,
+          //     ),
+          //     onPressed: _toggleFavorite),
+          // IconButton(
+          //     icon: Icon(
+          //       Icons.share,
+          //       color: Colors.grey[700],
+          //     ),
+          //     onPressed: () async {
+          //       QuoteController.shareQuote(lists, _currentIndex);
+          //     }),
+       // ],
         title: Text.rich(
           TextSpan(
               text: ';;',
-              style: TextStyle(
+              style: const TextStyle(
                   fontFamily: 'Cormorant',
                   fontWeight: FontWeight.w900,
                   fontSize: 30,
@@ -159,7 +159,7 @@ class _QuotesScreenState extends State<QuotesScreen> {
                           ),
                         )
                       : QuoteWidget(
-                          currentIndex: index ,
+                          currentIndex: index,
                           lists: state.quotes,
                           index: index,
                         );
@@ -170,70 +170,7 @@ class _QuotesScreenState extends State<QuotesScreen> {
                 child: Text(state.errorMessage),
               );
           }
-          // if (state is QuotesLoadedState) {
-          //   lists = state.quotes;
-          //   //lists = ApiServies.data ?? state.quotes;
 
-          //   listTage = ApiServies.tags;
-          //   print(listTage!.length);
-
-          //   return Column(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     children: [
-          //       // Container(
-          //       //     color: Colors.white,
-          //       //     height: 120,
-          //       //     child: Wrap(
-          //       //       children: listTage!
-          //       //           .map((e) => Padding(
-          //       //                 padding: const EdgeInsets.all(2.0),
-          //       //                 child: Chip(
-          //       //                   onDeleted: () {
-          //       //                     setState(() {
-          //       //                       listTage!.remove(e);
-          //       //                     });
-          //       //                     ;
-          //       //                   },
-          //       //                   label: Text('${e.name}'),
-          //       //                 ),
-          //       //               ))
-          //       //           .toList(),
-          //       //     )),
-          //       // SizedBox(
-          //       //   height: 25,
-          //       // ),
-          //       Expanded(
-          //         child: ListView.builder(
-          //           scrollDirection: Axis.vertical,
-          //           controller: _scrollController,
-          //           itemCount: lists!.length,
-          //           // onPageChanged: (index) {
-          //           //   setState(() {
-          //           //     _currentIndex = index;
-          //           //     _loadFavoriteQuoteStatus();
-          //           //   });
-          //           // },
-          //           itemBuilder: (context, index) {
-          //             return QuoteWidget(
-          //               currentIndex: index + 1,
-          //               lists: lists,
-          //               index: index,
-          //             );
-          //           },
-          //         ),
-          //       ),
-          //     ],
-          //   );
-          // } else if (state is QuotesErrorState) {
-          //   return const Center(
-          //     child: Text('Failed to fetch quotes.'),
-          //   );
-          // } else {
-          //   return const Center(
-          //     child: CircularProgressIndicator(),
-          //   );
-          // }
         },
       ),
     );
--- lib/presentation/widget/quoteWidget.dart
+++ lib/presentation/widget/quoteWidget.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quote/core/SharedPreferences.dart';
 import 'package:quote/core/fontStyle.dart';
 import 'package:quote/domain/quote.dart';
+import 'package:quote/presentation/widget/QuoteController.dart';
 
-class QuoteWidget extends StatelessWidget {
-  const QuoteWidget({
+class QuoteWidget extends StatefulWidget {
+  QuoteWidget({
     super.key,
     required int currentIndex,
     required this.lists,
@@ -15,53 +17,107 @@ class QuoteWidget extends StatelessWidget {
   final int index;
 
   @override
+  State<QuoteWidget> createState() => _QuoteWidgetState();
+}
+
+class _QuoteWidgetState extends State<QuoteWidget> {
+  bool _isFavorite = false;
+
+  Future<void> _loadFavoriteQuoteStatus() async {
+    setState(() {
+      _isFavorite =
+          SharedPrefController().getData(key: widget._currentIndex.toString());
+    });
+  }
+
+  void _toggleFavorite() async {
+    setState(() {
+      _isFavorite = !_isFavorite;
+      SharedPrefController()
+          .setData(widget._currentIndex.toString(), _isFavorite);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFavoriteQuoteStatus();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(5),
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            "No.",
-            style: FontStyle.cormorantStyle,
-            textAlign: TextAlign.end,
-          ),
-          Text(
-            " ______",
-            style: FontStyle.cormorantStyle
-                .copyWith(fontWeight: FontWeight.w300, color: Colors.grey),
-            textAlign: TextAlign.end,
-          ),
-          Text(
-            "${_currentIndex + 1}",
-            style: FontStyle.cormorantStyle.copyWith(
-              fontSize: 25,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "No.",
+              style: FontStyle.cormorantStyle,
+              textAlign: TextAlign.end,
             ),
-            textAlign: TextAlign.end,
-          ),
-          SelectableText(
-            lists![index].content!,
-            style: FontStyle.cormorantStyle
-                .copyWith(fontWeight: FontWeight.w600, color: Colors.black),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            " ______",
-            style: FontStyle.cormorantStyle
-                .copyWith(fontWeight: FontWeight.w300, color: Colors.grey),
-            textAlign: TextAlign.end,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            " ${lists![index].author}",
-            style: FontStyle.cormorantStyle
-                .copyWith(fontWeight: FontWeight.w400, color: Colors.black),
-            textAlign: TextAlign.end,
-          ),
-        ],
+            Text(
+              " ______",
+              style: FontStyle.cormorantStyle
+                  .copyWith(fontWeight: FontWeight.w300, color: Colors.grey),
+              textAlign: TextAlign.end,
+            ),
+            Text(
+              "${widget._currentIndex + 1}",
+              style: FontStyle.cormorantStyle.copyWith(
+                fontSize: 25,
+              ),
+              textAlign: TextAlign.end,
+            ),
+            SelectableText(
+              widget.lists![widget.index].content!,
+              style: FontStyle.cormorantStyle
+                  .copyWith(fontWeight: FontWeight.w600, color: Colors.black),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              " ______",
+              style: FontStyle.cormorantStyle
+                  .copyWith(fontWeight: FontWeight.w300, color: Colors.grey),
+              textAlign: TextAlign.end,
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  " ${widget.lists![widget.index].author}",
+                  style: FontStyle.cormorantStyle.copyWith(
+                      fontWeight: FontWeight.w400, color: Colors.black),
+                  textAlign: TextAlign.end,
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                        icon: Icon(
+                          _isFavorite ? Icons.favorite : Icons.favorite_border,
+                          color: Colors.red,
+                        ),
+                        onPressed: _toggleFavorite),
+                    IconButton(
+                        icon: Icon(
+                          Icons.share,
+                          color: Colors.grey[700],
+                        ),
+                        onPressed: () async {
+                          QuoteController.shareQuote(
+                              widget.lists, widget._currentIndex);
+                        }),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
